1f864106f82aac4b30e538dfd0693f643c1d8e4f
Ribulous Barnulous <ribulous.barnulous@gmail.com>
2015-08-26 06:18:10 -0400

    added make_* functions for homogenous data frames

--- include/echo/data_frame/homogenous_data_frame.h
+++ include/echo/data_frame/homogenous_data_frame.h
@@ -2,7 +2,11 @@
 
 #include <echo/data_frame/homogenous_data_frame_fwd.h>
 #include <echo/data_frame/homogenous_data_frame_accessor.h>
-#include <echo/k_array.h>
+#include <echo/data_frame/homogenous_data_frame_traits.h>
+#include <echo/data_frame/concept.h>
+#include <echo/data_frame/matrix_conversion.h>
+#include <echo/numeric_array.h>
+#include <echo/execution_context.h>
 
 namespace echo {
 namespace data_frame {
@@ -42,10 +46,70 @@ class HomogenousDataFrame<T, htl::Tuple<ColumnTags...>, Allocator>
 };
 
 //------------------------------------------------------------------------------
+// make_homogenous_data_frame
+//------------------------------------------------------------------------------
+template <class T, class ColumnTags, class Allocator = std::allocator<T>,
+          CONCEPT_REQUIRES(concept::column_tags<ColumnTags>() &&
+                           memory::concept::memory_backend<Allocator>())>
+auto make_homogenous_data_frame(index_t num_rows,
+                                const Allocator& allocator = Allocator()) {
+  return HomogenousDataFrame<T, ColumnTags, Allocator>(num_rows, allocator);
+}
+
+template <class T, class ColumnTags, class Allocator = std::allocator<T>,
+          CONCEPT_REQUIRES(concept::column_tags<ColumnTags>() &&
+                           memory::concept::memory_backend<Allocator>())>
+auto make_homogenous_data_frame(ColumnTags, index_t num_rows,
+                                const Allocator& allocator = Allocator()) {
+  return HomogenousDataFrame<T, ColumnTags, Allocator>(num_rows, allocator);
+}
+
+//------------------------------------------------------------------------------
 // NumericDataFrame
 //------------------------------------------------------------------------------
 template <class T, class ColumnTags>
 using NumericDataFrame =
     HomogenousDataFrame<T, ColumnTags, memory::SimdAllocator<T>>;
+
+//------------------------------------------------------------------------------
+// make_numeric_data_frame
+//------------------------------------------------------------------------------
+template <class Scalar, class ColumnTags,
+          class Allocator = memory::SimdAllocator<Scalar>,
+          CONCEPT_REQUIRES(execution_context::concept::scalar<Scalar>() &&
+                           concept::column_tags<ColumnTags>() &&
+                           memory::concept::memory_backend<Allocator>())>
+auto make_numeric_data_frame(index_t num_rows,
+                             const Allocator& allocator = Allocator()) {
+  return make_homogenous_data_frame<Scalar, ColumnTags>(num_rows, allocator);
+}
+
+template <class Scalar, class ColumnTags,
+          class Allocator = memory::SimdAllocator<Scalar>,
+          CONCEPT_REQUIRES(execution_context::concept::scalar<Scalar>() &&
+                           concept::column_tags<ColumnTags>() &&
+                           memory::concept::memory_backend<Allocator>())>
+auto make_numeric_data_frame(ColumnTags, index_t num_rows,
+                             const Allocator& allocator = Allocator()) {
+  return make_homogenous_data_frame<Scalar, ColumnTags>(num_rows, allocator);
+}
+
+template <
+    class ExecutionContext, class DataFrame,
+    CONCEPT_REQUIRES(
+        execution_context::concept::allocation_backend<ExecutionContext>() &&
+        execution_context::concept::expression_executer<ExecutionContext>() &&
+        concept::numeric_data_frame<uncvref_t<DataFrame>>())>
+auto make_numeric_data_frame(const ExecutionContext& execution_context,
+                             DataFrame&& data_frame) {
+  using Scalar = homogenous_data_frame_traits::value_type<uncvref_t<DataFrame>>;
+  auto result = make_numeric_data_frame<
+      Scalar, homogenous_data_frame_traits::column_tags<uncvref_t<DataFrame>>>(
+      get_num_rows(data_frame),
+      make_aligned_allocator<Scalar>(execution_context));
+  copy(execution_context, make_matrix_cview(data_frame),
+       make_matrix_view(result));
+  return result;
+}
 }
 }
